f8df5767d98cefd4e16f66b471f083e3645a12e3
noctislight <flush715@outlook.com>
2025-03-26 16:48:56 +0100

    phase 1 -> setup signals for ctrl c and ctrlquit

--- minishell.c
+++ minishell.c
@@ -6,14 +6,36 @@
 /*   By: fben-ham <fben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 14:17:26 by fben-ham          #+#    #+#             */
-/*   Updated: 2025/03/26 15:57:02 by fben-ham         ###   ########.fr       */
+/*   Updated: 2025/03/26 16:48:03 by fben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int g_received_signal = 0;
 
+void signal_handle(int sig)
+{
+    
+}
 
+void setup_signal()
+{
+    struct sigaction action1; // CTRL+C
+    struct sigaction action2; // CTRL+/
+    
+    sigemptyset(&action1.sa_mask);
+    action1.sa_flags = 0;
+    action1.sa_handler = signal_handle;
+    action1.sa_sigaction(SIGINT, &action1 ,NULL);
+
+    sigemptyset(&action2.sa_mask);
+    action1.sa_flags = 0;
+    action1.sa_handler = signal_handle;
+    action1.sa_sigaction(SIGQUIT, &action2,NULL);
+    
+
+}
 /*
 Main respectant uniquement la phase 1 pour le moment
 
@@ -25,6 +47,7 @@ int main()
 {
     char *rl;
 
+    setup_signal(); // regroupe sigaction pour les differents cas : ctrl+c et ctrl+/
     while(1)
     {
         rl = readline("MissionLocale> ");
--- minishell.h
+++ minishell.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <signal.h>
 
+extern int g_received_signal; // extend permet d'etendre la variable dans plusieurs fichiers sans redefinir constamment
 
 #endif
