cc0f989addee6de85c8506b63a25e503a40a58c9
noctislight <flush715@outlook.com>
2025-03-26 17:28:01 +0100

    phase 1 -> interactive and noninteractive signals handling

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: fben-ham <fben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 14:17:26 by fben-ham          #+#    #+#             */
-/*   Updated: 2025/03/26 16:48:03 by fben-ham         ###   ########.fr       */
+/*   Updated: 2025/03/26 17:27:28 by fben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,81 @@
 
 int g_received_signal = 0;
 
+/*
+Ctrl+c : nouvelle ligne, readline est informé, vide toute la ligne, reaffiche le prompt
+Ctrl+/ : rien ne se passe comme demandé dans le sujet
+*/
 void signal_handle(int sig)
 {
-    
+    if(sig == SIGINT)
+    {
+        g_received_signal = SIGINT;
+
+        write(1, "\n", 1);
+        rl_on_new_line();
+        rl_replace_line("", 0);
+        rl_redisplay();
+    }
 }
 
-void setup_signal()
+/*
+Setup le traitement des signaux pour ctrl c et / 
+*/
+void setup_signal(void)
 {
-    struct sigaction action1; // CTRL+C
-    struct sigaction action2; // CTRL+/
+    struct sigaction action;
     
-    sigemptyset(&action1.sa_mask);
-    action1.sa_flags = 0;
-    action1.sa_handler = signal_handle;
-    action1.sa_sigaction(SIGINT, &action1 ,NULL);
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    action.sa_handler = signal_handle;
+    sigaction(SIGINT, &action,NULL); // CTRL+C
 
-    sigemptyset(&action2.sa_mask);
-    action1.sa_flags = 0;
-    action1.sa_handler = signal_handle;
-    action1.sa_sigaction(SIGQUIT, &action2,NULL);
-    
-
+    signal(SIGQUIT, SIG_IGN);
 }
+
+/*
+Gestion au cas où une commande est en cours
+Mise à default
+*/
+void set_noninteractive_signal(void)
+{
+    struct sigaction action;
+
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    action.sa_handler = SIG_DFL;
+    sigaction(SIGINT, &action,NULL);
+    sigaction(SIGINT, &action,NULL);
+}
+
+
 /*
 Main respectant uniquement la phase 1 pour le moment
 
 Note que Break -> coupe en cas EOF
         ajouter a l'historique uniquement si la chaine n'est pas vide
-TODO : 
+TODO / Notes :
+- Si probleme avec l'interpret de RL pour les signaux -> rl_catch_signals = 0
+-  
 */
 int main()
 {
     char *rl;
 
-    setup_signal(); // regroupe sigaction pour les differents cas : ctrl+c et ctrl+/
+    
     while(1)
     {
+        setup_signal(); // regroupe sigaction pour les differents cas : ctrl+c et ctrl+/
         rl = readline("MissionLocale> ");
         if(!rl)
             break; // en cas de EOF (ctrl+d)
         if(*rl) // uniquement si pleine
+        {
+            set_noninteractive_signal();
             add_history(rl);
+        }
+
         free(rl);
-    }   
+    }  
     return (0);
 }
